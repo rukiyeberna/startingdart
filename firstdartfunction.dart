26593e40222e455419f3f687c58f6c934174ee36
rukiyeberna <117301103+rukiyeberna@users.noreply.github.com>
2022-12-12 17:41:10 +0300

    Add files via upload

---
+++ firstdartfunction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() 
+{  var name="I am starting to learn dart programming language for flutter.";
+  
+   printDart(name);
+}
+
+  printDart(String name)
+  {
+    print(name);
+  }
+
+
+
+
